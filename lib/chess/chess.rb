6e82194320fc5495f2e4ac23101e83bcdcd71814
himumi <erliansyaha@gmail.com>
2024-06-01 21:10:22 +0900

    refactor: create #new_piece in chess.rb

--- lib/chess/chess.rb
+++ lib/chess/chess.rb
@@ -1,8 +1,21 @@
 class Chess
   attr_accessor :board, :players
-  
+
   def initialize(board, players)
     @board = board
     @players = players
+  end
+
+  def new_piece(role, color, position)
+    piece = {
+      'king' => King.new(color, position),
+      'queen' => Queen.new(color, position),
+      'bishop' => Bishop.new(color, position),
+      'knight' => Knight.new(color, position),
+      'rook' => Rook.new(color, position),
+      'pawn' => Pawn.new(color, position)
+    }
+
+    piece[role]
   end
 end
