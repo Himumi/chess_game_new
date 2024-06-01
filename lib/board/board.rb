36f59ec2e834e828ede9ab5c0ccc4da11a588f60
himumi <erliansyaha@gmail.com>
2024-06-01 10:04:34 +0900

    refactor: create #print_board method in Board class

--- lib/board/board.rb
+++ lib/board/board.rb
@@ -3,4 +3,26 @@ class Board
   def initialize
     @board = Array.new(8) { Array.new(8) }
   end
+
+  def print_board
+    letters = "       A   B   C   D   E   F   G   H  "
+    dashed = "     ---------------------------------"
+
+    puts letters, dashed
+
+    board.each_with_index do |items, row|
+      line = "  #{8 - row}  |" # Add letter for each row
+      items.each_with_index do |item, column|
+        item.nil? ? line += '   |' : line += " #{item} |"
+
+        line += "  #{8 - row}" if column.equal?(items.length - 1) # Add letter after last column
+      end
+      puts line, dashed
+    end
+    puts letters
+  end
 end
+
+game = Board.new
+
+game.print_board
