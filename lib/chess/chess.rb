bf93ddff6694d290a1f5b9b98139c2c3acea653f
himumi <erliansyaha@gmail.com>
2024-06-02 11:49:33 +0900

    refactor: separate some methods into converter module
      separate #convert_to_indexes and #convert_to_key into new module file

--- lib/chess/chess.rb
+++ lib/chess/chess.rb
@@ -1,5 +1,8 @@
+require './lib/modules/converter.rb'
 class Chess
   attr_accessor :board, :players
+
+  include Converter
 
   def initialize(board, players)
     @board = board
@@ -17,45 +20,6 @@ class Chess
     }
 
     piece[role]
-  end
-
-  def convert_to_indexes(key)
-    letters = {
-      'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3,
-      'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7
-    }
-
-    numbers = {
-      '1' => 7, '2' => 6, '3' => 5, '4' => 4,
-      '5' => 3, '6' => 2, '7' => 1, '8' => 0
-    }
-
-    letter = letters[key[0]]
-    number = numbers[key[1]]
-
-    return nil if letter.nil? or number.nil?
-
-    [number, letter]
-  end
-
-  def convert_to_key(index)
-    return nil unless index.length.equal?(2)
-
-    letters = {
-      0 => 'a', 1 => 'b', 2 => 'c', 3 => 'd',
-      4 => 'e', 5 => 'f', 6 => 'g', 7 => 'h'
-    }
-
-    numbers = {
-      0 => '8', 1 => '7', 2 => '6', 3 => '5',
-      4 => '4', 5 => '3', 6 => '2', 7 => '1'
-    }
-    number = numbers[index[0]]
-    letter = letters[index[1]]
-
-    return nil if number.nil? or letter.nil?
-
-    "#{letter}#{number}"
   end
 
   def add_to_board
