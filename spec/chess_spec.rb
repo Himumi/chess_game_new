f1e6336f3b0a0c50db8818711bd56ec4e7d7f807
himumi <erliansyaha@gmail.com>
2024-06-02 09:09:22 +0900

    test: create #convert_to_indexes test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,5 +1,27 @@
 require './lib/chess/chess.rb'
 
 describe Chess do
-  
+  let(:board) { double('Board', board: Array.new(8)) }
+  let(:first_player) { double('Player', color: 'white') }
+  let(:second_player) { double('Player', color: 'black') }
+  subject(:game) { described_class.new(board, [first_player, second_player]) }
+  describe '#convert_to_indexes' do
+    it 'converts a1 to [0, 0]' do
+      converted = game.convert_to_indexes('a8')
+
+      expect(converted).to eq([0, 0])
+    end
+
+    it 'returns nil when key is invalid' do
+      converted = game.convert_to_indexes('j1')
+
+      expect(converted).to be_nil
+    end
+
+    it 'returns nil when key is more than 2' do
+      converted = game.convert_to_indexes('a-1')
+
+      expect(converted).to be_nil
+    end
+  end
 end
