03b33e266e346a4b8a9976b9ae1f64c764ec70c8
himumi <erliansyaha@gmail.com>
2024-06-02 09:49:25 +0900

    test: create #convert_to_key test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -24,4 +24,30 @@ describe Chess do
       expect(converted).to be_nil
     end
   end
+
+  describe '#convert_to_key' do
+    it 'converts [0, 0] to a8' do
+      converted = game.convert_to_key([0, 0])
+
+      expect(converted).to eq('a8')
+    end
+
+    it 'returns nil when indexes is invalid' do
+      converted = game.convert_to_key([9,0])
+
+      expect(converted).to be_nil
+    end
+
+    it 'returns nil when indexes is more than 2' do
+      converted = game.convert_to_key([0, 0, 0])
+
+      expect(converted).to be_nil
+    end
+
+    it 'returns nil when indexes is not 2' do
+      converted = game.convert_to_key([0])
+
+      expect(converted).to be_nil
+    end
+  end
 end
