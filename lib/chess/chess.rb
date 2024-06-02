14d0bf22f1c943031347ff851322a397f483a64d
himumi <erliansyaha@gmail.com>
2024-06-02 09:10:18 +0900

    refactor: create #convert_to_indexes in Chess class

--- lib/chess/chess.rb
+++ lib/chess/chess.rb
@@ -18,4 +18,23 @@ class Chess
 
     piece[role]
   end
+
+  def convert_to_indexes(key)
+    letters = {
+      'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3,
+      'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7
+    }
+
+    numbers = {
+      '1' => 7, '2' => 6, '3' => 5, '4' => 4,
+      '5' => 3, '6' => 2, '7' => 1, '8' => 0
+    }
+
+    letter = letters[key[0]]
+    number = numbers[key[1]]
+
+    return nil if letter.nil? or number.nil?
+
+    [letter, number]
+  end
 end
