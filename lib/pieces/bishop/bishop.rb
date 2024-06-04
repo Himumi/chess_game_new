fa9bc402676bd37474924c9ea2a3e0cd77a1ea36
himumi <erliansyaha@gmail.com>
2024-06-04 20:52:18 +0900

    refactor: create #direction in Bishop class

--- lib/pieces/bishop/bishop.rb
+++ lib/pieces/bishop/bishop.rb
@@ -10,4 +10,15 @@ class Bishop < Piece
     @symbol = (color == 'white' ? "\u265D" : "\u2657")
     @notation = 'B'
   end
+
+  def direction(position, way)
+    a = position[0]
+    b = position[1]
+    paths = [
+      [a-1, b-1], [a-1, b+1], [a+1, b+1], [a+1, b-1]
+    ]
+
+    return nil if paths[way].any? { |i| !i.between?(0, 7) }
+    paths[way]
+  end
 end
