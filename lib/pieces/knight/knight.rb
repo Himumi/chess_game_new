4ce264ce4271ae8548b6b0d2ef323f3446c20f2f
himumi <erliansyaha@gmail.com>
2024-06-04 21:35:07 +0900

    refactor: create #direction in Knight class

--- lib/pieces/knight/knight.rb
+++ lib/pieces/knight/knight.rb
@@ -10,4 +10,16 @@ class Knight < Piece
     @symbol = (color == 'white' ? "\u265E" : "\u2658")
     @notation = 'N'
   end
+
+  def direction(position, way)
+    a = position[0]
+    b = position[1]
+    paths = [
+      [a-1, b-2], [a-2, b-1], [a-2, b+1], [a-1, b+2],
+      [a+1, b+2], [a+2, b+1], [a+2, b-1], [a+1, b-2]
+    ]
+
+    return nil if paths[way].any? { |i| !i.between?(0, 7) }
+    paths[way]
+  end
 end
