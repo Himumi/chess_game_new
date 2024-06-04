144a7bcdd3bae9ee3b45760e888649b85d932b67
himumi <erliansyaha@gmail.com>
2024-06-04 21:39:37 +0900

    refactor: create #direction in Rook class

--- lib/pieces/rook/rook.rb
+++ lib/pieces/rook/rook.rb
@@ -10,4 +10,15 @@ class Rook < Piece
     @symbol = (color == 'white' ? "\u265C" : "\u2656")
     @notation = 'R'
   end
+
+  def direction(position, way)
+    a = position[0]
+    b = position[1]
+    paths = [
+      [a, b-1], [a-1, b], [a, b+1], [a+1, b]
+    ]
+
+    return nil if paths[way].any? { |i| !i.between?(0, 7) }
+    paths[way]
+  end
 end
