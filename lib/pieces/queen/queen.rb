6071c38626865c9a1e3096f6c80c27a2efb1329b
himumi <erliansyaha@gmail.com>
2024-06-04 20:47:51 +0900

    refactor: create #direction in Queen class

--- lib/pieces/queen/queen.rb
+++ lib/pieces/queen/queen.rb
@@ -10,4 +10,16 @@ class Queen < Piece
     @symbol = (color == 'white' ? "\u265B" : "\u2655")
     @notation = 'Q'
   end
+
+  def direction(position, way)
+    a = position[0]
+    b = position[1]
+    paths = [
+      [a, b-1], [a-1, b-1], [a-1, b], [a-1, b+1],
+      [a, b+1], [a+1, b+1], [a+1, b], [a+1, b-1]
+    ]
+
+    return nil if paths[way].any? { |i| !i.between?(0, 7) }
+    paths[way]
+  end
 end
