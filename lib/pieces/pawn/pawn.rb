91d4f365a0295f4a40c5e6e9e0c61855d9da2c0f
himumi <erliansyaha@gmail.com>
2024-06-29 11:03:37 +0900

    refactor: create #valid_direction in Pawn class

--- lib/pieces/pawn/pawn.rb
+++ lib/pieces/pawn/pawn.rb
@@ -9,5 +9,24 @@ class Pawn < Piece
     @position = position
     @symbol = (color == 'white' ? "\u265F" : "\u2659")
     @notation = ''
+    @moved = true
+  end
+
+  def valid_direction(position)
+    a = position[0]
+    b = position[1]
+
+    paths = {
+      'white' => [a - 1, b],
+      'black' => [a + 1, b]
+    }
+
+    return nil if paths[@color].any? { |i| !i.between?(0, 7) }
+
+    paths[@color]
   end
 end
+
+pawn = Pawn.new("black", "a1")
+
+p pawn.valid_direction([6,0])
